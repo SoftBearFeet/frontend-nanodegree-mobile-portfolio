0ca23da5303960cfd5bb18328fee61932319fffd
SoftBearFeet <SoftBearFeet@users.noreply.github.com>
2016-04-07 01:39:56 -0800

    setup 'gruntfile.js' to use 'grunt-responsive-images', it is not working though.

--- gruntfile.js
+++ gruntfile.js
@@ -2,6 +2,29 @@ module.exports = function(grunt) {
   require('load-grunt-tasks')(grunt);
 
   grunt.initConfig({
-    
+    responsive_images: {
+      options: {
+        engine: 'im'
+      },
+      myTask: {
+        options: {
+          sizes: [{
+            width: 320,
+          },{
+            width: 640,
+          }, {
+            width: 1024,
+          }]
+        },
+        files: [{
+          src: ['views/images/pizzeria.jpg'],
+          dest: 'dist/'
+        }]
+      }
+    }
   });
+
+  grunt.registerTask('default',[
+    'responsive_images'
+  ]);
 };
