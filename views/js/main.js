b9b5602f1cf4d71000479c65492f9d7372fd6701
SoftBearFeet <SoftBearFeet@users.noreply.github.com>
2016-04-19 20:39:55 -0800

    moved var outside of "updatePosition()" loop.

--- views/js/main.js
+++ views/js/main.js
@@ -502,16 +502,18 @@ function logAverageFrame(times) {   // times is the array of User Timing measure
 // The following code for sliding background pizzas was pulled from Ilya's demo found at:
 // https://www.igvita.com/slides/2012/devtools-tips-and-tricks/jank-demo.html
 
-// Moved "items" from the "updatePositions()" loop.
-  var items = document.getElementsByClassName('mover');
 
 // Moves the sliding background pizzas based on scroll position
 function updatePositions() {
   frame++;
   window.performance.mark("mark_start_frame");
 
+  // Moved "items" from the loop.
+  var items = document.getElementsByClassName('mover');
+  var top = (document.body.scrollTop / 1250);
+
   for (var i = 0; i < items.length; i++) {
-    var phase = Math.sin((document.body.scrollTop / 1250) + (i % 5));
+    var phase = Math.sin(top + (i % 5));
     items[i].style.left = items[i].basicLeft + 100 * phase + 'px';
   }
 
